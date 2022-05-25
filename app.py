8e97f09fb4d0f2576513e8a8bc2acb66f75323c8
mavismmg <contatomateusjorgejf@gmail.com>
2022-05-24 20:23:39 -0700

    feat: new functions

--- app.py
+++ app.py
@@ -14,54 +14,56 @@ from bar_query import api_serviceCount_per_state_df
 
 app = Dash(__name__)
 
-postgres_db = psycopg2.connect(
-    dbname='SGM',
-    user='postgres',
-    host='ec2-3-95-135-96.compute-1.amazonaws.com',
-    password='trlab2021'
-)
+app.layout = html.Div([
+    dcc.Graph(id='chropleth_plot'),
+    dcc.Graph(id='scatter_plot'),
+    dcc.Graph(id='bar_plot')
+])
 
-# @app.callback(
-#     Output('graph', 'figure'),
-#     Output('graph2', 'figure'),
-#     Input('slider', 'value')
-# )
-
+@app.callback(
+    Output('graph', 'figure'),
+    Output('graph2', 'figure'),
+    Input('slider', 'value'))
 def load_map():
     with urlopen('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson') as response:
-        brazil = json.load(response)
+        geo_map = json.load(response)
 
-    state_id_map = {}
+    # state_id_map = {}
 
-    for feature in brazil['features']:
-        feature['id'] = feature['properties']['name']
-        state_id_map[feature['properties']['sigla']] = feature['id']
+    # for feature in brazil['features']:
+    #     feature['id'] = feature['properties']['name']
+    #     state_id_map[feature['properties']['sigla']] = feature['id']
 
-    return brazil
+    return geo_map
 
 
-brazil = load_map()
+def chropleth_plot():
+    geo_map = load_map()
 
-fig = px.choropleth_mapbox(
-    geoapi_df, geojson=brazil, locations='state',
-    mapbox_style="carto-darkmatter", color='count',
-    center={"lat": -14.6633, "lon": -53.54627}, zoom=3)
+    fig = px.choropleth_mapbox(
+        geoapi_df, geojson=geo_map, locations='state',
+        mapbox_style="carto-darkmatter", color='count',
+        center={"lat": -14.6633, "lon": -53.54627}, zoom=3)
 
-#fig.update_geos(fitbounds="locations", visible=False)
-#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    #fig.update_geos(fitbounds="locations", visible=False)
+    #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-fig2 = px.scatter_mapbox(
-        api_service_per_state_df, lat=api_service_per_state_df['latitude'], lon=api_service_per_state_df['longitude'],
-        color=api_service_per_state_df['geoapi'], mapbox_style="carto-darkmatter"
-    )
+    return fig
 
-fig3 = px.bar(api_serviceCount_per_state_df, x='state', y='count', color='geoapi_id')
 
-app.layout = html.Div([
-    dcc.Graph(id='graph', figure=fig),
-    dcc.Graph(id='graph2', figure=fig2),
-    dcc.Graph(id='graph3', figure=fig3)
-])
+def scatter_plot():
+    fig = px.scatter_mapbox(
+            api_service_per_state_df, lat=api_service_per_state_df['latitude'], lon=api_service_per_state_df['longitude'],
+            color=api_service_per_state_df['geoapi'], mapbox_style="carto-darkmatter"
+        )
+
+    return fig
+
+
+def bar_plot():
+    fig = px.bar(api_serviceCount_per_state_df, x='state', y='count', color='geoapi_id')
+
+    return fig
 
 
 if __name__ == '__main__':
