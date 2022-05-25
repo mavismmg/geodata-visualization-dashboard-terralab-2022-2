4eee003c19060f6557fe20a7cf1c26f5f788dc42
mavismmg <contatomateusjorgejf@gmail.com>
2022-05-24 20:24:14 -0700

    feat: class Bar

--- bar_query.py
+++ bar_query.py
@@ -2,37 +2,61 @@ import pandas as pd
 
 from db import Connection
 
-db_connect = Connection()
+class Bar:
+    def __init__(self, db_connect=Connection()):
+        self.db_connect = db_connect
+        self.data = []
 
-conn = db_connect.connection()
+    def access_db(self):        
+        postgres_data = self.db_connect.connection()
 
-cur = conn.cursor()
+        cur = postgres_data.cursor()
 
-cur.execute(
-    '''select
-    "state",
-    "geoapi_id",
-    count(geolocation_id)
-    from "Response"
-    group by "state",
-    "geoapi_id"
-    ''', conn)
+        cur.execute(
+            '''select
+            "state",
+            "geoapi_id",
+            count(geolocation_id)
+            from "Response"
+            group by "state",
+            "geoapi_id"''', postgres_data)
 
-data = cur.fetchall()
+        self.data = cur.fetchall()
 
-temp_df = pd.DataFrame(data, columns=['state', 'geoapi_id', 'count'])
+        return self.data
 
-temp_df = temp_df.apply(lambda x: x.astype(str).str.lower())
 
-temp_df['state'] = temp_df['state'].str.replace('state of ', '')
-temp_df['state'] = temp_df['state'].str.replace('federal district', 'distrito federal')
-temp_df['state'] = temp_df['state'].str.replace('í', 'i')
-temp_df['state'] = temp_df['state'].str.replace('á', 'a')
-temp_df['state'] = temp_df['state'].str.replace('ã', 'a')
-temp_df['state'] = temp_df['state'].str.replace('ô', 'o')
+    def api_serviceCount_per_state(self):
+        df = pd.DataFrame(self.data, columns=['state', 'geoapi_id', 'count'])
 
-pd.set_option('display.max_rows', None)
+        df = df.apply(lambda x: x.astype(str).str.lower())
 
-temp_df = temp_df.drop(labels=[8, 58, 98, 133, 134, 139])
+        replace_dict = {
+            'state of ': '',
+            'federal district': 'distrito federal',
+            'í': 'i',
+            'á': 'a',
+            'ã': 'a',
+            'ô': 'o'
+        }
 
-api_serviceCount_per_state_df = pd.DataFrame(temp_df, columns=['state', 'geoapi_id', 'count'])
+        for state in df:
+            df[state] = df[state].str.replace(replace_dict)
+
+        # temp_df['state'] = temp_df['state'].str.replace('state of ', '')
+        # temp_df['state'] = temp_df['state'].str.replace('federal district', 'distrito federal')
+        # temp_df['state'] = temp_df['state'].str.replace('í', 'i')
+        # temp_df['state'] = temp_df['state'].str.replace('á', 'a')
+        # temp_df['state'] = temp_df['state'].str.replace('ã', 'a')
+        # temp_df['state'] = temp_df['state'].str.replace('ô', 'o')
+
+        pd.set_option('display.max_rows', None)
+
+        df = df.drop(labels=[8, 58, 98, 133, 134, 139])
+
+        api_serviceCount_per_state_df = pd.DataFrame(df, columns=[
+                                                         'state',
+                                                         'geoapi_id',
+                                                         'count'])
+
+        return api_serviceCount_per_state_df
