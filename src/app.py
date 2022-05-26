b67fb79f26c328d4ee970e8bdb2fad3f67a9f94b
mavismmg <contatomateusjorgejf@gmail.com>
2022-05-26 16:10:49 -0700

    refactor: class geo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-from datetime import date
 from dash import Dash, dcc, html, Input, Output
 
 import plotly.graph_objects as go
@@ -6,118 +5,66 @@ import plotly.express as px
 import json
 
 from urllib.request import urlopen
-from query.choropleth_query import Chropleth
-from query.scattermatplot_query import Scatter
-from query.bar_query import Bar
-from query.date_query import Date
+from choropleth_query import Chropleth
+from scattermatplot_query import Scatter
+from bar_query import Bar
+#from date_query import Date
 
 app = Dash(__name__)
 
-date = Date().api_date()
+class Geo:
+    def __init__(self,
+        chropleth_df=Chropleth().geoapi(),
+        scatter_df=Scatter().api_service_per_state(),
+        bar_df=Bar().api_serviceCount_per_state()):
+
+        self.chropleth_df = chropleth_df
+        self.scatter_df = scatter_df
+        self.bar_df = bar_df
+
+    
+    @staticmethod
+    def load_map():
+        with urlopen('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson') as response:
+            geo_map = json.load(response)
+
+        state_id_map = {}
+
+        for feature in geo_map['features']:
+            feature['id'] = feature['properties']['name']
+            state_id_map[feature['properties']['sigla']] = feature['id']
+
+        return geo_map
+
+    
+    def update_figure(self):
+        geo_map = Geo().load_map()
+
+        map_fig = px.choropleth_mapbox(
+            self.chropleth_df, geojson=geo_map, locations='Estado',
+            mapbox_style="carto-positron", color='Buscas concluídas',
+            center={"lat": -14.6633, "lon": -53.54627}, zoom=3)
+
+        scatter_fig = px.scatter_mapbox(
+                self.scatter_df, lat=self.scatter_df['latitude'],
+                lon=self.scatter_df['longitude'], color=self.scatter_df['geoapi'], 
+                mapbox_style="carto-positron", size="geoapi", hover_name="geoapi"
+            )
+
+        bar_fig = px.bar(self.bar_df, x='Estado', y='Geocodificações concluídas', color='Geoapi_id',
+                         text='Geocodificações concluídas')
+
+        return map_fig, scatter_fig, bar_fig
+
+
+map_fig, scatter_fig, bar_fig = Geo().update_figure()
 
 app.layout = html.Div([
-    dcc.Graph(id='chropleth_plot'),
-    dcc.Graph(id='scatter_plot'),
-    dcc.Graph(id='bar_plot'),
-    dcc.Graph(id='test_plot'),
-    # dcc.Slider(
-    #     date['date'].min(),
-    #     date['date'].max(),
-    #     step=None,
-    #     value=date['date'].min(),
-    #     marks={str(date): str(date) for date in date['date'].unique()},
-    #     id='date-slider'
-    # )
+    dcc.Graph(id='chropleth_plot', figure=map_fig),
+    dcc.Graph(id='scatter_plot', figure=scatter_fig),
+    dcc.Graph(id='bar_plot', figure=bar_fig),
+    #dcc.Graph(id='test_plot'),
 ])
-
-# @app.callback(
-#     Output('chropleth_plot', 'figure'),
-#     Output('scatter_plot', 'figure'),
-#     Output('bar_plot', 'figure'),
-#     Output('test_plot', 'figure'),
-#     Input('date-slider', 'value'))
-# def update_figure(selected_date):
-#     geo_map = load_map()
-
-#     ch_df = Chropleth().geoapi()
-
-#     fig = px.choropleth_mapbox(
-#         ch_df, geojson=geo_map, locations='state',
-#         mapbox_style="carto-darkmatter", color='count',
-#         center={"lat": -14.6633, "lon": -53.54627}, zoom=3)
-
-#     filtered_df = date[date.date == selected_date]
-    
-#     filtered_scatter = px.scatter(filtered_df, x='geoapi_id', y='request_id',
-#                                   color='date')
-
-#     filtered_scatter.update_layout(transition_duration=500)
-
-#     sc = Scatter().api_service_per_state()
-
-#     scatter = px.scatter_mapbox(
-#             sc, lat=sc['latitude'], lon=sc['longitude'],
-#             color=sc['geoapi'], mapbox_style="carto-darkmatter"
-#         )
-
-#     ba = Bar().api_serviceCount_per_state()
-#     bar = px.bar(ba, x='state', y='count', color='geoapi_id')
-
-#     return fig, filtered_scatter, scatter, bar
-
-@staticmethod
-def load_map():
-    with urlopen('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson') as response:
-        geo_map = json.load(response)
-
-    # state_id_map = {}
-
-    # for feature in brazil['features']:
-    #     feature['id'] = feature['properties']['name']
-    #     state_id_map[feature['properties']['sigla']] = feature['id']
-
-    return geo_map
-
-
-# def test_plot(selected_date):
-#     filtered_df = date[date.date == selected_date]
-
-#     fig = px.scatter(filtered_df, x='geoapi_id', y='request_id',
-#                      color='date')
-
-#     fig.update_layout(transition_duration=500)
-
-#     return fig
-
-
-def chropleth_plot():
-    df = Chropleth().geoapi()
-    geo_map = load_map()
-
-    fig = px.choropleth_mapbox(
-        df, geojson=geo_map, locations='state',
-        mapbox_style="carto-darkmatter", color='count',
-        center={"lat": -14.6633, "lon": -53.54627}, zoom=3)
-
-    return fig
-
-
-def scatter_plot():
-    df = Scatter().api_service_per_state()
-    fig = px.scatter_mapbox(
-            df, lat=df['latitude'], lon=df['longitude'],
-            color=df['geoapi'], mapbox_style="carto-darkmatter"
-        )
-
-    return fig
-
-
-def bar_plot():
-    df = Bar().api_serviceCount_per_state()
-    fig = px.bar(df, x='state', y='count', color='geoapi_id')
-
-    return fig
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
